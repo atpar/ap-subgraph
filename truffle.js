a0f8266a073cc20eed80b85f56fd6ff4b524b83e
jo-es <escherich.johannes+github@gmail.com>
2020-10-22 17:51:09 +0200

    Remove truffle artifacts

--- truffle.js
+++ truffle.js
@@ -1,25 +1,4 @@
-require('babel-register')
-require('babel-polyfill')
-
-const HDWalletProvider = require('truffle-hdwallet-provider')
-
 module.exports = {
-  networks: {
-    development: {
-      host: '127.0.0.1',
-      port: 8545,
-      network_id: '*',
-    },
-    goerli: {
-      provider: function() {
-        return new HDWalletProvider(
-          process.env.MNEMONIC,
-          `https://goerli.infura.io/v3/${process.env.GOERLI_INFURA_API_KEY}`
-        )
-      },
-      network_id: '5',
-    },
-  },
   compilers: {
     solc: {
       version: "0.6.11",
